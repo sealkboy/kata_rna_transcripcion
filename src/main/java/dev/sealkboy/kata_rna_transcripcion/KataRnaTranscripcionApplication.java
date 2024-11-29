8346fe16beedb67252baaab7580b92c0ce5ee7af
unknown <sealkevinboyabad@gmail.com>
2024-11-29 12:38:10 -0800

    Primer commit

---
+++ src/main/java/dev/sealkboy/kata_rna_transcripcion/KataRnaTranscripcionApplication.java
@@ -0,0 +1,13 @@
+package dev.sealkboy.kata_rna_transcripcion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataRnaTranscripcionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataRnaTranscripcionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/sealkboy/kata_rna_transcripcion/KataRnaTranscripcionApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sealkboy.kata_rna_transcripcion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataRnaTranscripcionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
